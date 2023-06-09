dcbb71c60ffb75878a6cdda15eb86b7fe6e44854
GeonWoo Park <gomudayya@gmail.com>
2023-06-10 00:56:14 +0900

    .
    .

--- booksale/src/test/java/dbteam4/booksale/repository/PostMapperTest.java
+++ booksale/src/test/java/dbteam4/booksale/repository/PostMapperTest.java
@@ -1,8 +1,12 @@
 package dbteam4.booksale.repository;
 
+import dbteam4.booksale.domain.Post;
+import dbteam4.booksale.dto.PostBookDTO;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,6 +17,20 @@ class PostMapperTest {
     private PostMapper postMapper;
     @Test
     void findByPostId() {
-        postMapper.findByPostId(13L); //상태 좋아요 직거래로 팝니다.
+        PostBookDTO postBookDTO = postMapper.findByPostId(13L);
+
+        System.out.println("postBookDTO = " + postBookDTO);
+
+    }
+
+    @Test
+    void findRecentPost() {
+        List<PostBookDTO> recentPosts = postMapper.findRecentPost(3);
+
+        for (PostBookDTO post : recentPosts) {
+            System.out.println("post = " + post);
+        }
+
+
     }
 }
