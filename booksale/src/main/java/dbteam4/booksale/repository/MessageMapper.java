1820cc6cd26f782462892eca56699056a79c6972
GeonWoo Park <gomudayya@gmail.com>
2023-06-12 07:53:16 +0900

    .
    .

---
+++ booksale/src/main/java/dbteam4/booksale/repository/MessageMapper.java
@@ -0,0 +1,16 @@
+package dbteam4.booksale.repository;
+
+import dbteam4.booksale.dto.MessageDTO;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface MessageMapper {
+    void save(MessageDTO messageDTO);
+
+    List<MessageDTO> findByTwoId(Long user1Id, Long user2Id);
+
+    List<MessageDTO> findById(Long userId);
+
+}
